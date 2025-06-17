801f1b02a0855c983732b798dc98547c76fb7d77
skuze <skuzevanov@gmail.com>
2025-06-17 09:26:22 +0500

    Отключил мидлвар "approve" для непроверенных юзеров (чтобы они могли редактировать свой профиль).

--- routes/profile.php
+++ routes/profile.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::prefix('/profile')->group(function () {
+Route::prefix('/profile')->withoutMiddleware('approve')->group(function () {
     Route::get('', [App\Http\Controllers\UsersController::class, 'profile'])->name('profile');
     Route::put('', [App\Http\Controllers\UsersController::class, 'profileUpdate'])->name('profile.update');
 });
