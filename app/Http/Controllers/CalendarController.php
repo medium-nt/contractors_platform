f0aeda5816a7fe0833b7ea6209701604b60e4f8b
skuze <skuzevanov@gmail.com>
2025-07-02 21:41:00 +0500

    Тестим баг на проде.

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -14,7 +14,10 @@ class CalendarController extends Controller
 
         return view('calendar.index', [
             'title' => 'Календарь',
-            'events' => $tasks->merge($tasksOrders)->merge($orders)
+//            'events' => $tasks->merge($tasksOrders)->merge($orders),
+            'events' => collect($tasks)
+                ->merge(collect($tasksOrders))
+                ->merge(collect($orders)),
         ]);
     }
 }
