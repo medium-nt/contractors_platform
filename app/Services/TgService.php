d92c7e3de0be13f66cb9c7842e3f565f90c11c58
skuze <skuzevanov@gmail.com>
2025-08-17 07:38:43 +0500

    Закомментировал код отправки сообщений в Telegram для сервера разработки

--- app/Services/TgService.php
+++ app/Services/TgService.php
@@ -19,22 +19,22 @@ class TgService
 
         //  если development сервер, то отправляем сообщение в телеграм через guzzle.
         if (!app()->environment('production')) {
-            $client = new Client([
-                'verify' => false,
-                'timeout' => 30,
-                'connect_timeout' => 30,
-                'curl' => [
-                    CURLOPT_SSL_VERIFYPEER => false,
-                    CURLOPT_SSL_VERIFYHOST => false,
-                ]
-            ]);
-
-            $client->post('https://api.telegram.org/bot' . config('telegram.bots.mybot.token') . '/sendMessage', [
-                'form_params' => [
-                    'chat_id' => $chatId,
-                    'text' => $message
-                ]
-            ]);
+//            $client = new Client([
+//                'verify' => false,
+//                'timeout' => 30,
+//                'connect_timeout' => 30,
+//                'curl' => [
+//                    CURLOPT_SSL_VERIFYPEER => false,
+//                    CURLOPT_SSL_VERIFYHOST => false,
+//                ]
+//            ]);
+//
+//            $client->post('https://api.telegram.org/bot' . config('telegram.bots.mybot.token') . '/sendMessage', [
+//                'form_params' => [
+//                    'chat_id' => $chatId,
+//                    'text' => $message
+//                ]
+//            ]);
         } else {
             try {
                 Telegram::sendMessage([
